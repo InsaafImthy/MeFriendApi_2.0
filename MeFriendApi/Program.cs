fd8845eb86761f317d036d18c83fca5e47d916ec
Insaaf Imthiyas <inu.imthy2004@gmail.com>
2026-07-14 07:54:47 +0530

    Refactor CORS configuration and update appsettings for improved security

--- MeFriendApi/Program.cs
+++ MeFriendApi/Program.cs
@@ -1,30 +1,40 @@
 using MeFriendApi.Services;
 using MeFriendApi.Services.Middlewares;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
-ServiceRegistration.RegisterService(builder.Services);
-builder.Services.AddControllers();
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
+ ServiceRegistration.RegisterService(builder.Services);
+ builder.Services.AddControllers();
+ builder.Services.AddAuthentication();
+ builder.Services.AddAuthorization();
+ builder.Services.AddEndpointsApiExplorer();
+ builder.Services.AddSwaggerGen();
+ builder.Services.AddCors(options =>
+ {
+ options.AddPolicy("AllowReactApp", policy =>
+ {
+ policy.AllowAnyOrigin()
+ .AllowAnyHeader()
+ .AllowAnyMethod();
+ });
+ });
+ var app = builder.Build();
+ 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+ 
+app.UseSwagger();
+ app.UseSwaggerUI();
+ 
+app.UseCors("AllowReactApp");
+ 
 app.UseHttpsRedirection();
-
+ 
 app.UseAuthentication();
-app.UseAuthorization();
-
+ app.UseAuthorization();
+ 
 app.UseMiddleware<UserAuthMiddleware>();
-
+ 
 app.MapControllers();
-
+ 
 app.Run();
+ 
+ 
